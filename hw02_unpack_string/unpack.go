d7dae82c1ca3b30563dde99f2118eeb2361b3c78
lkupriyanova <lkupriyanova@ozon.ru>
2022-06-20 00:08:40 +0300

    initial solution

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,36 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	var b strings.Builder
+	inputLen := len(input)
+	for i := 0; i < inputLen-1; i++ {
+		current, next := rune(input[i]), rune(input[i+1])
+		if unicode.IsDigit(current) {
+			if unicode.IsDigit(next) || i == 0 {
+				return "", ErrInvalidString
+			}
+			continue
+		}
+		if unicode.IsDigit(next) {
+			digit, err := strconv.Atoi(string(next))
+			if err != nil {
+				return "", errors.New("error while doing Atoi")
+			}
+			b.WriteString(strings.Repeat(string(current), digit))
+			continue
+		}
+		b.WriteRune(current)
+	}
+	if inputLen > 0 {
+		b.WriteByte(input[len(input)-1])
+	}
+	return b.String(), nil
 }
